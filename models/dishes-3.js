4d490a3f6fc40f1669542602ea2b8161ffd18db9
dsabalete <dsabalete@gmail.com>
2016-05-18 10:38:27 +0000

    Variation 3 with comments in the dishes

---
+++ models/dishes-3.js
@@ -0,0 +1,44 @@
+// grab things we need
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, { 
+    timestamps: true
+});
+
+// create schema
+var dishSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    comments: [commentSchema] // array of comments
+}, {
+    timestamps: true
+});
+
+// the schema is useless so far 
+// we need to create a model using it
+var Dishes = mongoose.model("Dish", dishSchema);
+
+module.exports = Dishes;
