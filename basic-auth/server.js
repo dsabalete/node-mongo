7035985727218c2566dd2f361de12f911c3f3ea3
dsabalete <dsabalete@gmail.com>
2016-06-13 15:54:45 +0000

    adding cookies - server side dev

--- basic-auth/server.js
+++ basic-auth/server.js
@@ -1,37 +1,47 @@
 var express = require('express');
 var morgan = require('morgan');
+var cookieParser =  require('cookie-parser');
 
-var hostname = 'localhost';
-var port = 3000;
+var hostname = process.env.IP || 'localhost';
+var port = process.env.PORT || 3000;
 
 var app = express();
 
 app.use(morgan('dev'));
+app.use(cookieParser('12345-67890-09876-54321')); // secret key
 
 function auth(req, res, next) {
     console.log(req.headers);
 
-    var authHeader = req.headers.authorization;
-    console.log(authHeader);
-
-    if (!authHeader) {
-        var err = new Error('You are not authenticated!!!');
-        err.status = 401;
-        next(err);
-        return;
-    }
-    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-    console.log(auth);
-
-    var user = auth[0];
-    var pass = auth[1];
-
-    if (user == 'admin' && pass == 'password') {
-        next(); // authorized
-    } else {
-        var err = new Error('You are not authenticated!');
-        err.status = 401;
-        next(err);
+    if (!req.signedCookies.user) { // there is no signed cookie
+        var authHeader = req.headers.authorization;
+        if (!authHeader) {
+            var err = new Error('You are not authenticated!!!');
+            err.status = 401;
+            next(err);
+            return;
+        }
+        var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+        console.log(auth);
+        var user = auth[0];
+        var pass = auth[1];
+        if (user == 'admin' && pass == 'password') { // if user is admin and 'pass' is 'password' we set a cookie
+            res.cookie('user','admin', {signed:true});
+            next(); // authorized
+        } else {
+            var err = new Error('You are not authenticated!');
+            err.status = 401;
+            next(err);
+        }
+    } else { // there is a signed cookie
+        if (req.signedCookies.user === 'admin') { // the cookie is from user admin
+            console.log(req.signedCookies);
+            next();
+        } else {
+            var err = new Error('You are not authenticated!');
+            err.status = 401;
+            next(err);
+        }
     }
 }
 
